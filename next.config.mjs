767aaf4aeeaa72c57e17fcea4aba8911bc8fb4f6
dkagypro-sys <dkagypro@gmail.com>
2026-09-25 15:19:03 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,9 @@
-const nextConfig = { reactStrictMode: true }; export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
